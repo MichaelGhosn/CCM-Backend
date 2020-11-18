96b0df1b3912cf66da09996cb8fe899cfff2176a
Michael Ghosn <45416282+MichaelGhosn@users.noreply.github.com>
2020-11-18 16:57:48 +0200

    Add Reservation adjust response description grammar

--- CCM.Application/Reservation/Command/Add/AddReservationHandler.cs
+++ CCM.Application/Reservation/Command/Add/AddReservationHandler.cs
@@ -133,7 +133,7 @@ namespace CCM.Application.Reservation.Command.Add
             return new ResponseModel<AddReservationResponseModel>()
             {
                 Success = true,
-                Description = "Revervation was successfull, " + (alreadyBookedSeatsDuringThatTime + 1) + " booked of " + mapTotalCapacity + " available during that time"
+                Description = "Reservation was successful , " + (alreadyBookedSeatsDuringThatTime + 1) + " booked of " + mapTotalCapacity + " available during that time"
                 
             };
             
